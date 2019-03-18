7a7fb80a78fc6bcd9686096b608d49192ff02371
Камилла <48441041+Kamilla56@users.noreply.github.com>
2019-03-18 17:24:04 +0300

    Create test_rebro.java

---
+++ test_rebro.java
@@ -0,0 +1,25 @@
+public class test_Rebro{
+   public static void main(String[] args) { 
+     Method b = new Method();
+    /* b.MethodAdd(new Rebro(1,2,1));
+     b.MethodAdd(new Rebro(2,3,3));
+     b.MethodAdd(new Rebro(3,4,3));
+     b.MethodAdd(new Rebro(4,5,2));
+     b.MethodAdd(new Rebro(5,6,1));
+     b.MethodAdd(new Rebro(1,6,1));
+     b.MethodAdd(new Rebro(3,6,1));
+     b.MethodAdd(new Rebro(2,7,1));*/
+     b.MethodAdd(new Rebro(2,3,1));
+     b.MethodAdd(new Rebro(5,4,1));
+     b.MethodAdd(new Rebro(3,5,1));
+     b.MethodAdd(new Rebro(5,2,1));
+     b.MethodAdd(new Rebro(4,3,1));
+     b.MethodAdd(new Rebro(1,5,1));
+     b.MethodAdd(new Rebro(3,1,1));
+     b.MethodAdd(new Rebro(1,2,1));
+     b.MethodAdd(new Rebro(4,1,1));
+     b.MethodAdd(new Rebro(2,1,1));
+   b.Alg_Kruskala();
+   b.Kosarayu();
+   }
+}
